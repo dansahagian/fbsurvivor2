20bd6018104cc7b73fe388c55ebd0019b4c07a07
Dan Sahagian <dan@dansahagian.com>
2020-04-10 16:01:23 -0700

    Adds teams to conftest

--- fbsurvivor/core/tests/conftest.py
+++ fbsurvivor/core/tests/conftest.py
@@ -20,12 +20,21 @@ PST = pytz.timezone("US/Pacific")
 
 @pytest.fixture(autouse=True)
 def players(db):
-    return PlayerFactory.create_batch(size=2)
+    return PlayerFactory.create_batch(
+        size=2,
+        username=factory.Iterator(["Automator", "Roboto"]),
+        link=factory.Iterator(["ABC123", "DEF456"])
+    )
 
 
 @pytest.fixture(autouse=True)
 def seasons(db):
-    return SeasonFactory.create_batch(size=2, year=factory.Iterator([2019, 2020]))
+    return SeasonFactory.create_batch(
+        size=2,
+        year=factory.Iterator([2019, 2020]),
+        is_locked=factory.Iterator([True, False]),
+        is_current=factory.Iterator([False, True])
+    )
 
 
 @pytest.fixture(autouse=True)
@@ -35,13 +44,21 @@ def weeks(db, seasons):
     lw = now - datetime.timedelta(days=7)
 
     return WeekFactory.create_batch(
-        size=5, season=seasons[1], lock_datetime=factory.Iterator([lw, lw, lw, lw, nw])
+        size=5,
+        season=seasons[1],
+        week_num=factory.Iterator([1, 2, 3, 4, 5]),
+        lock_datetime=factory.Iterator([lw, lw, lw, lw, nw])
     )
 
 
 @pytest.fixture(autouse=True)
 def teams(db, seasons):
-    return TeamFactory.create_batch(size=5, season=seasons[1])
+    return TeamFactory.create_batch(
+        size=5,
+        season=seasons[1],
+        team_code=factory.Iterator(["NE", "SF", "TB", "GB", "DAL"]),
+        bye_week=factory.Iterator([1, 2, 3, 4, 5])
+    )
 
 
 @pytest.fixture(autouse=True)
--- fbsurvivor/core/tests/views/test_pick.py
+++ fbsurvivor/core/tests/views/test_pick.py
@@ -12,7 +12,20 @@ def test_get_player_info_and_context(players, seasons, player_statuses):
     assert "player_status" in c
 
 
-def test_view_picks(db, client, players):
-    url = reverse("picks", args=[players[0].link, 2020])
+def test_view_picks(db, client):
+    url = reverse("picks", args=["ABC123", 2020])
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+def test_view_pick_week_is_locked(db, client):
+    url = reverse("pick", args=["ABC123", 2020, 1])
+    response = client.get(url)
+    assert response.status_code == 302
+    assert response.url == "/picks/ABC123/2020/"
+
+
+def test_view_pick(db, client):
+    url = reverse("pick", args=["ABC123", 2020, 5])
     response = client.get(url)
     assert response.status_code == 200

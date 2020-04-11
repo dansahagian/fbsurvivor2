31861a4273ac5e83a78435f4620ff0446e8f01e5
Dan Sahagian <dan@dansahagian.com>
2020-04-10 21:08:49 -0700

    Fixes urls

--- fbsurvivor/core/urls.py
+++ fbsurvivor/core/urls.py
@@ -5,8 +5,8 @@ from fbsurvivor.core import views
 urlpatterns = [
     path("", views.signup, name="home"),
     path("forgot/", views.forgot, name="forgot"),
-    path("<str:link>/", views.player_redirect, name="player_redirect"),
-    path("<str:link>", views.player_redirect, name="player_redirect"),
+    path("board/<str:link>/", views.player_redirect, name="player_redirect"),
+    path("board/<str:link>", views.player_redirect, name="player_redirect"),
     path("board/<str:link>/<int:year>/", views.player, name="player"),
     path("manager/<str:link>/<int:year>/", views.manager, name="manager"),
     path("results/<str:link>/<int:year>/", views.results, name="results"),

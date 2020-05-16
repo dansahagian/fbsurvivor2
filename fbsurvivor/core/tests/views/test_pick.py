f1e8292017c84b29772467c007f416c954dccd4f
Dan Sahagian <dan@dansahagian.com>
2020-05-16 15:23:37 -0700

    Clean up tests for pick views

--- fbsurvivor/core/tests/views/test_pick.py
+++ fbsurvivor/core/tests/views/test_pick.py
@@ -2,6 +2,18 @@ from django.urls import reverse
 
 from fbsurvivor.core.views.pick import get_player_info_and_context
 
+import pytest
+
+
+@pytest.fixture
+def link(players):
+    return players[0].link
+
+
+@pytest.fixture
+def year(seasons):
+    return seasons[1].year
+
 
 def test_get_player_info_and_context(players, seasons, player_statuses):
     p1 = players[0]
@@ -18,22 +30,20 @@ def test_get_player_info_and_context(players, seasons, player_statuses):
     assert "player_status" in context
 
 
-def test_view_picks(db, client, players, seasons):
-    link = players[0].link
-    year = seasons[1].year
+def test_view_picks(client, link, year):
     url = reverse("picks", args=[link, year])
     response = client.get(url)
     assert response.status_code == 200
 
 
-def test_view_pick_week_is_locked(db, client):
-    url = reverse("pick", args=["ABC123", 2020, 1])
+def test_view_pick_week_is_locked(client, link, year):
+    url = reverse("pick", args=[link, year, 1])
     response = client.get(url)
     assert response.status_code == 302
-    assert response.url == "/picks/ABC123/2020/"
+    assert response.url == f"/picks/{link}/{year}/"
 
 
-def test_view_pick_get(db, client):
-    url = reverse("pick", args=["ABC123", 2020, 5])
+def test_view_pick_get(client, link, year):
+    url = reverse("pick", args=[link, year, 5])
     response = client.get(url)
     assert response.status_code == 200

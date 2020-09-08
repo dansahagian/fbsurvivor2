5594483f69e3ee52493147710fe1d5b934a97ab1
Dan Sahagian <dan@dansahagian.com>
2020-09-08 10:09:36 -0700

    Black formatting

--- fbsurvivor/core/migrations/0014_auto_20200908_1005.py
+++ fbsurvivor/core/migrations/0014_auto_20200908_1005.py
@@ -6,13 +6,17 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0013_auto_20200530_1508'),
+        ("core", "0013_auto_20200530_1508"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='pick',
-            name='result',
-            field=models.CharField(choices=[('W', 'WIN'), ('L', 'LOSS'), ('R', 'RETIRED'), (None, 'None')], max_length=1, null=True),
+            model_name="pick",
+            name="result",
+            field=models.CharField(
+                choices=[("W", "WIN"), ("L", "LOSS"), ("R", "RETIRED"), (None, "None")],
+                max_length=1,
+                null=True,
+            ),
         ),
     ]

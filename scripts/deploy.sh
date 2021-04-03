16105cd3ed86d990d9d7b20bcd495718866d7599
Dan Sahagian <dan@dansahagian.com>
2021-04-02 21:38:22 -0700

    Use pipenv to run pytest

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-if black --check . && pytest .
+if black --check . && pipenv run pytest .
 then
   echo "Deploying..."
   ssh dan@fbsurvivor.com "/opt/fbsurvivor2/scripts/deploy_on_server.sh"

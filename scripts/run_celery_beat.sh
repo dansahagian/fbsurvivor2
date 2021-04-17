dd5d0db7302bd61ec6b91a68c83e047c0f5c57f7
Dan Sahagian <dan@dansahagian.com>
2021-04-16 23:31:27 -0700

    Update the run scripts to use venv

--- scripts/run_celery_beat.sh
+++ scripts/run_celery_beat.sh
@@ -2,4 +2,4 @@ DATE=`date '+%Y-%m-%d %H:%M:%S'`
 echo "fbsurvivor celery worker started at ${DATE}" | systemd-cat -p info
 
 cd /opt/fbsurvivor2
-pipenv run celery -A fbsurvivor beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler
+venv/bin/celery -A fbsurvivor beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler
--- scripts/run_wsgi_server.sh
+++ scripts/run_wsgi_server.sh
@@ -2,4 +2,4 @@ DATE=`date '+%Y-%m-%d %H:%M:%S'`
 echo "fbsurvivor started at ${DATE}" | systemd-cat -p info
 
 cd /opt/fbsurvivor2
-pipenv run gunicorn fbsurvivor.wsgi
+venv/bin/gunicorn fbsurvivor.wsgi

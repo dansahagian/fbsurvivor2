ed376e36ea86768c395714c44b9896d7a72cc588
Dan Sahagian <dan@dansahagian.com>
2021-04-17 12:49:59 -0700

    Move debug toolbar middleware

--- fbsurvivor/settings.py
+++ fbsurvivor/settings.py
@@ -54,7 +54,6 @@ MIDDLEWARE = [
     "django.middleware.gzip.GZipMiddleware",
     "htmlmin.middleware.HtmlMinifyMiddleware",
     "htmlmin.middleware.MarkRequestMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -63,6 +62,9 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+if DEBUG:
+    MIDDLEWARE.append("debug_toolbar.middleware.DebugToolbarMiddleware")
 
 ROOT_URLCONF = "fbsurvivor.urls"
 

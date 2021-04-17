5a592b80d3265e21e46e12a9fe0b12ab6b1febc6
Dan Sahagian <dan@dansahagian.com>
2021-04-16 23:48:07 -0700

    Make it executable

--- scripts/run_celery_beat.sh
+++ scripts/run_celery_beat.sh
@@ -2,4 +2,4 @@ DATE=`date '+%Y-%m-%d %H:%M:%S'`
 echo "fbsurvivor celery worker started at ${DATE}" | systemd-cat -p info
 
 cd /opt/fbsurvivor2
-venv/bin/celery -A fbsurvivor beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler
+./venv/bin/python -m celery -A fbsurvivor beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler
--- scripts/run_wsgi_server.sh
+++ scripts/run_wsgi_server.sh
@@ -2,4 +2,4 @@ DATE=`date '+%Y-%m-%d %H:%M:%S'`
 echo "fbsurvivor started at ${DATE}" | systemd-cat -p info
 
 cd /opt/fbsurvivor2
-venv/bin/gunicorn fbsurvivor.wsgi
+./venv/bin/python -m gunicorn fbsurvivor.wsgi

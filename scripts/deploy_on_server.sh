5add32d81baa7d4ec6e7ad7e577fa3caf00d184e
Dan Sahagian <dan@dansahagian.com>
2021-04-16 23:19:28 -0700

    Switch to virtualenv and proxies

--- scripts/deploy_on_server.sh
+++ scripts/deploy_on_server.sh
@@ -8,11 +8,11 @@ sudo systemctl stop celerybeat.service
 echo "Deploying..."
 cd /opt/fbsurvivor2
 git pull origin main
-pipenv install
+venv/bin/pip install -r ./requirements/production.txt
 
-pipenv run ./manage.py migrate
-pipenv run ./manage.py collectstatic --no-input
-pipenv run ./manage.py check --deploy
+venv/bin/python ./manage.py migrate
+venv/bin/python ./manage.py collectstatic --no-input
+venv/bin/python ./manage.py check --deploy
 
 echo "Starting services..."
 sudo systemctl start fbsurvivor.service

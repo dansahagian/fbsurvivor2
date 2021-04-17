fb24644bf19386e0ffc3089bc4ad828aa95d8180
Dan Sahagian <dan@dansahagian.com>
2021-04-16 23:31:27 -0700

    Update the run scripts to use venv

--- scripts/run_celery_worker.sh
+++ scripts/run_celery_worker.sh
@@ -2,4 +2,4 @@ DATE=`date '+%Y-%m-%d %H:%M:%S'`
 echo "fbsurvivor celery worker started at ${DATE}" | systemd-cat -p info
 
 cd /opt/fbsurvivor2
-pipenv run celery -A fbsurvivor worker -l INFO
+venv/bin/celery -A fbsurvivor worker -l INFO

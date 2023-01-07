a12b078915c1140dfb6337dbc0c061383dba34f9
Dan Sahagian <dan@dansahagian.com>
2023-01-07 13:14:26 -0800

    Add a management command to pick the lottery winner

---
+++ fbsurvivor/core/management/commands/pick_lottery_winner.py
@@ -0,0 +1,30 @@
+from secrets import choice
+
+from django.core.management.base import BaseCommand
+
+from fbsurvivor.core.models import Player, Season
+
+
+class Command(BaseCommand):
+    help = "Pick the winner of free entry for next season"
+
+    def handle(self, *args, **options):
+        current_season = Season.objects.get(is_current=True)
+
+        # filter out players who retired, won survivor, and me
+        players = Player.objects.filter(
+            playerstatus__season=current_season,
+            playerstatus__is_retired=False,
+            playerstatus__is_survivor=False,
+        ).exclude(username="DanTheAutomator")
+
+        # filter out players who missed picks during the season
+        ep = [
+            p.username
+            for p in players
+            if not p.pick_set.filter(team__isnull=True, week__season=current_season)
+        ]
+        display = "\n".join(ep)
+
+        print(f"\n\nEligible Players:\n\n{display}\n\n")
+        print(f"And the winner is... {choice(ep)}\n\n")

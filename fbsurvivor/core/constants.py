dc60da039f7ad353e42f44fa4f562a9aa8565475
Dan Sahagian <dan@dansahagian.com>
2023-08-13 16:49:01 -0700

    Simplify

---
+++ fbsurvivor/core/constants.py
@@ -0,0 +1,7 @@
+ROBOTS_TXT_CONTENT = """\
+User-Agent: *
+Disallow: /
+
+User-agent: GPTBot
+Disallow: /
+"""
--- fbsurvivor/urls.py
+++ fbsurvivor/urls.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
+from django.views.decorators.http import require_GET
 
 from fbsurvivor import settings
+from fbsurvivor.core.constants import ROBOTS_TXT_CONTENT
+
+
+@require_GET
+def robots_txt(
+    request,
+):
+    return HttpResponse(ROBOTS_TXT_CONTENT, content_type="text/plain")
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("robots.txt", robots_txt),
     path("", include("fbsurvivor.core.urls")),
 ]
 
